a622f0f53a3cb92b7fc7a50637f336da07e7c31d
Jano <jano.mutschler@outlook.com>
2026-02-06 10:39:00 +0100

    added first version of ex00

--- ex00/inc/whatever.hpp
+++ ex00/inc/whatever.hpp
@@ -6,7 +6,25 @@
 /*   By: jmutschl <jmutschl@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/06 09:58:08 by jmutschl          #+#    #+#             */
-/*   Updated: 2026/02/06 09:58:09 by jmutschl         ###   ########.fr       */
+/*   Updated: 2026/02/06 10:31:34 by jmutschl         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef WHATEVER_HPP
+#define WHATEVER_HPP
+
+template <typename T>
+void swap(T& a, T& b)
+{
+	T	temp = a;
+	a = b;
+	b = temp;
+}
+
+template <typename T>
+T min(T a, T b) { return (a < b ? a : b); }
+
+template <typename T>
+T max(T a, T b) { return (a > b ? a : b); }
+
+#endif
--- ex00/src/main.cpp
+++ ex00/src/main.cpp
@@ -6,7 +6,30 @@
 /*   By: jmutschl <jmutschl@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/06 09:58:11 by jmutschl          #+#    #+#             */
-/*   Updated: 2026/02/06 09:58:12 by jmutschl         ###   ########.fr       */
+/*   Updated: 2026/02/06 10:38:25 by jmutschl         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+#include <string>
+#include "whatever.hpp"
+
+int main( void )
+{
+	int a = 2;
+	int b = 3;
+	::swap( a, b );
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+
+	std::string c = "chaine1";
+	std::string d = "chaine2";
+
+	::swap(c, d);
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+
+	return (0);
+}
