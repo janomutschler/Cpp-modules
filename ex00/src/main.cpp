46b18d9f1484ad2ca3fa0857bb89149d399c16a0
Jano Mutschler <jmutschl@3-G-1.42heilbronn.de>
2026-02-12 11:13:34 +0100

    small design updates

--- ex00/src/main.cpp
+++ ex00/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jmutschl <jmutschl@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/02 10:14:46 by jmutschl          #+#    #+#             */
-/*   Updated: 2026/02/04 17:42:36 by jmutschl         ###   ########.fr       */
+/*   Updated: 2026/02/12 11:07:02 by jmutschl         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,6 @@ static void run(const std::string& lit)
 
 int main(int argc, char** argv)
 {
-    // If user provides args, test those only
     if (argc > 1)
     {
         for (int i = 1; i < argc; ++i)
@@ -32,48 +31,40 @@ int main(int argc, char** argv)
         return 0;
     }
 
-    // --- Char inputs ---
     run("a");
     run("Z");
     run("!");
-    run("0");        // should be int, not char
-    run(" ");        // space as char (displayable)
-    // Note: non-displayable char inputs are not supposed to be provided by subject.
+    run("0");
+    run(" ");
 
-    // --- Int inputs ---
     run("42");
     run("-42");
     run("+42");
-    run("1");        // int
-    run("127");      // DEL (non-displayable when cast to char)
-    run("31");       // non-displayable char
-    run("32");       // space
-    run("-2");       // char: impossible
+    run("1");
+    run("127");
+    run("31");
+    run("32");
+    run("-2");
 
-    // INT boundaries (assumes 32-bit int as usual at 42; still useful)
-    run("2147483647");   // INT_MAX
-    run("-2147483648");  // INT_MIN
-    run("2147483648");   // just above INT_MAX -> int impossible
-
-    // too large for long on many systems -> should become invalid (all impossible)
-    run("999999999999999999999999999999999999");
+    run("2147483647");
+    run("-2147483648");
+    run("2147483648");
 
     // --- Double inputs ---
     run("42.0");
     run("-42.0");
     run("0.0");
     run("1.5");
-    run("127.0");    // char boundary
-    run("128.0");    // char impossible
-    run("-0.0");     // check formatting/behavior
+    run("127.0");
+    run("128.0");
 
     // --- Float inputs ---
     run("42.0f");
     run("-42.0f");
     run("0.0f");
     run("1.5f");
-    run("127.112466f");
-    run("128.3678997f");
+    run("127.0f");
+    run("128.0f");
 
     // --- Pseudo literals ---
     run("nan");
@@ -93,12 +84,10 @@ int main(int argc, char** argv)
     run("1..0");
     run("1.0ff");
     run("f");
-    run(".");        // depending on your validator: likely invalid
+    run(".");
     run("..");
     run("+-42");
     run("42-");
-    run(" 42");      // leading whitespace (invalid)
-    run("42 ");      // trailing whitespace (invalid)
 
     return 0;
 }
